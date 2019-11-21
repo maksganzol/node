7144b2304e2c4f9aa6a72d9ef1f45d2266105738
maksganzol <tsurkini04@gmail.com>
2019-11-21 18:25:49 +0200

    last

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
+console.log("The request process may take several minutes, please wait... ");
 getMillionDigits();
 
 
 function GET(count, resp){
 
-    var request = require('request');//ИСПРАВИТЬ НА  10 000!!!!!!!!
-    request('https://www.random.org/integers/?num=2&min=0&max=1&col=1&base=10&format=plain&rnd=new', function (error, response, body) {
-        if(count<=10){
+    var request = require('request');
+    request('https://www.random.org/integers/?num=10000&min=0&max=1&col=1&base=10&format=plain&rnd=new', function (error, response, body) {
+        if(count<=100){
             count++;
             resp = resp.concat(response.body);
             GET(count, resp)
@@ -22,7 +23,7 @@ function showData(resp){
         var str = resp;
         var seq = getSequences2DArray();
             
-        console.log(resp.length)
+        console.log(resp.length + " digits were generated.")
         for(let i = 0; i < seq.length; i++){
             for(let j = 0; j < seq[i].length; j++){
                 count = getCountOfSequence(seq[i][j], str);
@@ -49,57 +50,53 @@ function Create2DArray(rows) {
   return arr;
 }
 function getSequences2DArray(){
-	var sequences = Create2DArray(Math.pow(2, 3));
-	for(let i = 1; i <=3; i++){
-		var countOfNumbers = Math.pow(2, i);
-		for(let j = 0; j < countOfNumbers; j++){
-			sequences[i][j] = intToBinary(j, i);//j.toString(2);
-		}
-	}
-	return sequences;
+  var sequences = Create2DArray(Math.pow(2, 3));
+  for(let i = 1; i <=3; i++){
+    var countOfNumbers = Math.pow(2, i);
+    for(let j = 0; j < countOfNumbers; j++){
+      sequences[i][j] = intToBinary(j, i);//j.toString(2);
+    }
+  }
+  return sequences;
 }
 
 function intToBinary(number, countOfDigits){
     var str = number.toString(2);
-	var length = str.length;
-	for(let i = 0; i < countOfDigits - length; i++){
-		str = "0" + str;
-	}
-	return str;
+  var length = str.length;
+  for(let i = 0; i < countOfDigits - length; i++){
+    str = "0" + str;
+  }
+  return str;
 }
 
 function getFrequencyInPersent(count, commonCount){
-	return Math.floor((count/commonCount)*100)
+  return Math.floor((count/commonCount)*100)
 }
 
 function getCountOfAllSequences(lengthOfSequences, str){
-	var summ = 0;
-	for(let i = 0; i < Math.pow(2, lengthOfSequences); i++){
-		sequen = intToBinary(i, lengthOfSequences);
-		count = getCountOfSequence(sequen, str);
-		summ += count;
-	}
-	return summ;
+  var summ = 0;
+  for(let i = 0; i < Math.pow(2, lengthOfSequences); i++){
+    sequen = intToBinary(i, lengthOfSequences);
+    count = getCountOfSequence(sequen, str);
+    summ += count;
+  }
+  return summ;
 }
 
 function getCountOfSequence(seq, str){
     var count = 0;
     flag = false;
-	for(let i = 0; i < str.length; i++){
-		for(let j = 0; j < seq.length; j++){
-			if(str[i+j]!=seq[j]){
+  for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < seq.length; j++){
+      if(str[i+j]!=seq[j]){
                 flag = false;
                 break;
-			}
-			flag = true;
+      }
+      flag = true;
         }
         if(flag) {
             count++;
         }
-	}
-	return count;
+  }
+  return count;
 }
-
-
-
-
